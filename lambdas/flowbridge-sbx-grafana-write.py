66b7b990bc207991b7d028a8b2c2c051e7fc89d2
euginirudayaraj <euginirudayaraj2002@gmail.com>
2026-06-03 15:41:00 +0530

    Test Lambda auto deploy

--- lambdas/flowbridge-sbx-grafana-write.py
+++ lambdas/flowbridge-sbx-grafana-write.py
@@ -1,10 +1,32 @@
 import json
+import logging
+from datetime import datetime, timezone
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def lambda_handler(event, context):
-    print("url-lambda event:", json.dumps(event))
+    """
+    FlowBridge SBX Grafana Write Lambda.
+    This is a sample Lambda handler for testing GitHub -> CodeBuild -> Lambda deployment.
+    """
+
+    logger.info("flowbridge-sbx-grafana-write invoked")
+    logger.info("Event: %s", json.dumps(event))
+
+    response_body = {
+        "message": "flowbridge-sbx-grafana-write Lambda deployed successfully",
+        "lambda_name": "flowbridge-sbx-grafana-write",
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "received_event": event
+    }
+
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "url lambda working eugin test"
-        })
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(response_body)
     }
